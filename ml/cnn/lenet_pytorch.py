721a1b2dce0eaa909ac955a688b4820fbf349684
krzepka <krzepka@users.noreply.github.com>
2022-06-22 19:11:17 +0200

    looks like first epoch was run

--- ml/cnn/lenet_pytorch.py
+++ ml/cnn/lenet_pytorch.py
@@ -1,5 +1,6 @@
 from argparse import ArgumentParser
 
+import numpy
 import numpy as np
 import torch
 
@@ -13,10 +14,10 @@ from ml.image_batch_generator import ImageBatchGenerator
 class LeNetTorch(nn.Module):
     """ Based on: https://www.kaggle.com/code/usingtc/lenet-with-pytorch/script """
 
-    def __init__(self):
+    def __init__(self, img_width, img_height):
         super(LeNetTorch, self).__init__()
-        self.conv1 = nn.Conv2d(1, 6, (5, 5), padding=2)
-        self.conv2 = nn.Conv2d(6, 16, (5, 5))
+        self.conv1 = nn.Conv2d(1, 1, (5, 5), padding=2)
+        self.conv2 = nn.Conv2d(1, 16, (5, 5))
         self.fc1 = nn.Linear(16 * 5 * 5, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 2)
@@ -41,12 +42,12 @@ class LeNetTorch(nn.Module):
 class LeNetTorchWrapper:
     """ Based on: https://www.kaggle.com/code/usingtc/lenet-with-pytorch/script """
 
-    def __init__(self, epochs, steps_per_epoch, validation_steps, use_gpu=True):
+    def __init__(self, epochs, steps_per_epoch, validation_steps, img_w=28, img_h=28, use_gpu=True):
         super().__init__()
         self.epochs = epochs
         self.steps_per_epoch = steps_per_epoch
         self.validation_steps = validation_steps
-        self.model = LeNetTorch()
+        self.model = LeNetTorch(img_w, img_h)
         self.use_gpu = use_gpu
 
         self.criterion = nn.CrossEntropyLoss()
@@ -90,7 +91,7 @@ class LeNetTorchWrapper:
             self.optimizer.step()
 
             if (epoch + 1) % epoch_info_freq == 0:
-                print("Epoch = %d, Loss = %f" % (epoch + 1, mini_loss.data[0]))
+                print(f"Epoch = {epoch+1}, Loss = {mini_loss.item()}")
 
     def save(self, path):
         raise NotImplementedError
@@ -102,6 +103,13 @@ class LeNetTorchWrapper:
     def predict(self, test_input):
         raise NotImplementedError
         # return self.model.predict(test_input)
+
+
+def prepare_data_for_torch(train_x: numpy.ndarray, train_y: numpy.ndarray):
+    return np.array(train_x).reshape(100000, 1, 28, 28), \
+        np.array(train_y).reshape(100000, 1)
+
+    pass
 
 
 def main():
@@ -120,6 +128,7 @@ def main():
 
     train_x, train_y = train_gen.read_input(0)
     train_x, train_y = train_gen.reshape_batch_data(train_x, train_y)
+    train_x, train_y = prepare_data_for_torch(train_x, train_y)
     net = LeNetTorchWrapper(epochs=1, steps_per_epoch=10, validation_steps=10, use_gpu=False)
     net.train(np.array(train_x), np.array(train_y))
 
